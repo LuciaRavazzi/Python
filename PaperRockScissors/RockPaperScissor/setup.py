c2b03d8b8086e24f3aa630a9cf17366a62d3b0a1
Lucia Ravazzi <ravazzilucia1@gmail.com>
2021-09-25 12:45:55 +0200

    My project

---
+++ PaperRockScissors/RockPaperScissor/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='RockPaperScissor',
+    version='0.0.1',
+    description='Rock, paper, scissor game.',
+    author='Lucia Ravazzi',
+    install_requires=[
+        'random',
+        'time'
+    ],
+    packages=find_packages()
+)
