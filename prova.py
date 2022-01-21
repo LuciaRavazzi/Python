8d89605f47e20b0f62ed827b26c05305b7815d07
Lucia Ravazzi <ravazzilucia1@gmail.com>
2022-01-21 19:04:34 +0100

    Prova

---
+++ prova.py
@@ -0,0 +1,9 @@
+def np_pep_code(a, b):
+    print("commit between double quotes")
+    if a % 2 == 0:
+        print("even")
+        return a * 2 * b
+    else:
+        print("odd")
+        c = 5
+        return c * a * b
